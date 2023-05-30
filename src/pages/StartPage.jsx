5745006437b9345f5ac0a6ec8c865eb9d80e8db3
Sagar <Sagar Wagdare>
2023-05-30 12:24:30 +0530

    Navbar added

---
+++ src/pages/StartPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../css/StartPage.css";
+import Header from "../components/Header";
+const StartPage = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+
+export default StartPage;
